d4ac67525ba2701912bec456293097f4a274db56
Vladimir Konovalov <vladimir74rus@yandex.ru>
2022-06-15 03:10:52 +0500

    implemented MealRestController

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
-import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -19,10 +18,6 @@ public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private final Map<Integer, Map<Integer, Meal>> usersMeals = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
-
-    {
-        MealsUtil.meals.forEach(meal -> save(meal, SecurityUtil.authUserId()));
-    }
 
     @Override
     public Meal save(Meal meal, int userId) {
@@ -51,6 +46,7 @@ public class InMemoryMealRepository implements MealRepository {
     public Collection<Meal> getAll(int userId) {
         Map<Integer, Meal> meals = usersMeals.get(userId);
         return meals == null || meals.isEmpty() ? Collections.emptyList() : meals.values().stream()
+                .filter(m -> m.getId().equals(userId))
                 .sorted(Comparator.comparing(Meal::getDateTime).reversed())
                 .collect(Collectors.toList());
     }
