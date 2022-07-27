cd9ae35bc45f888fac185ccfad2f69a7c80d7deb
Vladimir Konovalov <vladimir74rus@yandex.ru>
2022-07-27 14:09:47 +0500

    hw7 add MealRestController

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+class MealRestControllerTest extends AbstractControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void createWithLocation() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getBetween() {
+    }
+}
